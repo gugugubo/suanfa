a338c7007e2ac04f63c56098e7c4e2ea3f821b7d
guchunbo <1195550354@qq.com>
2020-10-27 11:52:24 +0800

    feat(timu190): 位运算

---
+++ 02-Sorting-Basic/Course Code (Java)/01-Selection-Sort/src/bobo/algo/likou/weiyunsuan/timu693.java
@@ -0,0 +1,22 @@
+package bobo.algo.likou.weiyunsuan;
+
+/**
+ * @Author 古春波
+ * @Description 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 5
+ * 输出：true
+ * 解释：5 的二进制表示是：101
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-number-with-alternating-bits
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Date 2020/10/27 11:51
+ * @Version 1.0
+ **/
+public class timu693 {
+}
