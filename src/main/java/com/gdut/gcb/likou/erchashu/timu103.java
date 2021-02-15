e3d99669718629fe8ba5ba19d145ae66638fc551
guchunbo <1195550354@qq.com>
2021-02-15 23:56:29 +0800

    feat(dongtaiguihua): 正则表达式

---
+++ src/main/java/com/gdut/gcb/likou/erchashu/timu103.java
@@ -0,0 +1,74 @@
+package com.gdut.gcb.likou.erchashu;
+
+import com.gdut.gcb.niuke.erchashu.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author 古春波
+ * @Description 103. 二叉树的锯齿形层序遍历
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 返回锯齿形层序遍历如下：
+ *
+ * [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Date 2021/2/15 23:26
+ * @Version 1.0
+ **/
+public class timu103 {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        boolean times = false;  // true 表示从左到右，否则从右到左
+        //按层遍历即可
+        //1.
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+        //2.
+        List<TreeNode> nodes = new ArrayList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            int size = nodes.size();
+            times = !times;
+            LinkedList<Integer> list = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode remove = nodes.remove(0);
+                if (!times){
+                    list.offerFirst(remove.val);
+                }else {
+                    list.offerLast(remove.val);
+                }
+               
+                if (remove.left != null) {
+                    nodes.add(remove.left);
+                }
+                if (remove.right != null) {
+                    nodes.add(remove.right);
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+    
+}
